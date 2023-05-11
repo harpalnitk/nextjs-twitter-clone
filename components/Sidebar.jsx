d702ba9f53704aa1ed52ce28f540a519b9345cd3
Harpal Singh <harpalnitk@gmail.com>
2023-05-11 15:24:54 +0530

    started firebase-auth

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,76 +1,99 @@
-import Image from "next/image";
-import SidebarMenuItem from "./SidebarMenuItem";
-import {  HomeIcon } from "@heroicons/react/24/solid";
-import { BellIcon, BookmarkIcon, ClipboardIcon, EllipsisHorizontalCircleIcon, EllipsisHorizontalIcon, HashtagIcon, InboxIcon, UserIcon } from "@heroicons/react/24/outline";
-import { useSession,signIn,signOut } from "next-auth/react";
-export default function Sidebar() {
+import Image from 'next/image';
+import SidebarMenuItem from './SidebarMenuItem';
+import { HomeIcon } from '@heroicons/react/24/solid';
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  EllipsisHorizontalCircleIcon,
+  EllipsisHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
+import { getAuth } from 'firebase/auth';
+// import { useSession,signIn,signOut } from "next-auth/react";
+const auth = getAuth();
 
-    const {data:session} = useSession();
+
+export default function Sidebar() {
+  // const {data:session} = useSession();
   return (
-    // fixed makes sidebar menu positioned at top using scrolling of feed 
+    // fixed makes sidebar menu positioned at top using scrolling of feed
     //feed component will go outside of original flexbox because of this position
     //fixed used in sidebar
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
-        {/* Twitter Logo  */}
-        <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
-            <Image 
-            src='/twitter_logo.png'
-            width={50}
-            height={50}
-            alt='logo'>
+    <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24'>
+      {/* Twitter Logo  */}
+      <div className='hoverEffect p-0 hover:bg-blue-100 xl:px-1'>
+        <Image
+          src='/twitter_logo.png'
+          width={50}
+          height={50}
+          alt='logo'
+        ></Image>
+      </div>
 
-            </Image>
-        </div>
-
-        {/* Menu  */}
-        <div className='mt-4 mb-2.5 xl:items-start'>
-            <SidebarMenuItem text='Home' Icon={HomeIcon} active></SidebarMenuItem>
-            <SidebarMenuItem text='Explore' Icon={HashtagIcon}></SidebarMenuItem>
-            {session && (
-                <>
-                <SidebarMenuItem text='Notifications' Icon={BellIcon}></SidebarMenuItem>
- <SidebarMenuItem text='Messages' Icon={InboxIcon}></SidebarMenuItem>
- <SidebarMenuItem text='Bookmark' Icon={BookmarkIcon}></SidebarMenuItem>
- <SidebarMenuItem text='Lists' Icon={ClipboardIcon}></SidebarMenuItem>
- <SidebarMenuItem text='Profile' Icon={UserIcon}></SidebarMenuItem>
- <SidebarMenuItem text='More' Icon={EllipsisHorizontalCircleIcon}></SidebarMenuItem>
-                </>
- 
-            )}
-           
-        </div>
-
-        
-        {session ? (
-            <>
-        {/* Tweet Button  */}
-        <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
-
-
-        {/* MiniProfile  */}
-        <div className='hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto'>
-            <Image 
-            src={session.user.image} 
-            width='100'
-             height='100' 
-             alt='user-image'
-             className="h-10 w-10 rounded-full xl:mr-2"
-             onClick={signOut}
-             />
-            <div className='leading-5 hidden xl:inline'>
-                <h4 className="font-bold">{session.user.name}</h4>
-                <p className="text-gray-500">@{session.user.username}</p>
-            </div>
-            <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
-        </div>
-            </>
-        ) : (
-            <button 
-            onClick={signIn}
-            className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"> Sign In</button>
+      {/* Menu  */}
+      <div className='mt-4 mb-2.5 xl:items-start'>
+        <SidebarMenuItem text='Home' Icon={HomeIcon} active></SidebarMenuItem>
+        <SidebarMenuItem text='Explore' Icon={HashtagIcon}></SidebarMenuItem>
+        {session && (
+          <>
+            <SidebarMenuItem
+              text='Notifications'
+              Icon={BellIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem text='Messages' Icon={InboxIcon}></SidebarMenuItem>
+            <SidebarMenuItem
+              text='Bookmark'
+              Icon={BookmarkIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem
+              text='Lists'
+              Icon={ClipboardIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem text='Profile' Icon={UserIcon}></SidebarMenuItem>
+            <SidebarMenuItem
+              text='More'
+              Icon={EllipsisHorizontalCircleIcon}
+            ></SidebarMenuItem>
+          </>
         )}
+      </div>
 
+      {session ? (
+        <>
+          {/* Tweet Button  */}
+          <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>
+            Tweet
+          </button>
 
+          {/* MiniProfile  */}
+          <div className='hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto'>
+            <Image
+              src={session.user.image}
+              width='100'
+              height='100'
+              alt='user-image'
+              className='h-10 w-10 rounded-full xl:mr-2'
+              onClick={signOut}
+            />
+            <div className='leading-5 hidden xl:inline'>
+              <h4 className='font-bold'>{session.user.name}</h4>
+              <p className='text-gray-500'>@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalIcon className='h-5 xl:ml-8 hidden xl:inline' />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className='bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'
+        >
+          {' '}
+          Sign In
+        </button>
+      )}
     </div>
-  )
+  );
 }
--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,8 +1,48 @@
 
 import Image from "next/image";
-import {getProviders, signIn} from 'next-auth/react';
+// import {getProviders, signIn} from 'next-auth/react';
+import {getAuth, signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
+import { db } from "@/firebase";
+import { useRouter } from "next/router";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 
-export default function SignIn({providers}) {
+
+export default function SignIn() {
+
+    const router = useRouter();
+
+    const onGoogleClick = async ()=> {
+  try {
+    
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+    const user = auth.currentUser.providerData[0];
+    //console.log('user in signin', user)
+    const docRef = doc(db,'twitter-users',user.uid);
+    //check if user already exists
+    const docSnap = await getDoc(docRef);
+    //console.log('docSnap', docSnap);
+    if(!docSnap.exists()){
+     // console.log('docSnap does not exists', docSnap)
+      await setDoc(docRef,{
+        name:user.displayName,
+        email:user.email,
+        userImg: user.photoURL,
+        uid:user.uid,
+        timestamp: serverTimestamp(),
+        username:user.displayName.split(' ').join('').toLocaleLowerCase()
+  
+      })
+    }
+    router.push('/');
+  
+  } catch (error) {
+    console.log(error)
+    
+  }
+    }
+
   return (
     <div className="flex justify-center mt-20 space-x-4">
         <Image 
@@ -12,8 +52,8 @@ export default function SignIn({providers}) {
         alt='twitter display image'
         className="hidden md:inline-flex object-cover md:w-44 md:h-80 rotate-6"/>
         <div className=''>
-            {Object.values(providers).map(provider => (
-                <div key={provider.name} className='flex flex-col items-center'>
+           
+                <div  className='flex flex-col items-center'>
                     <Image
                     src='/twitter-logo-large.png'
                     alt='twitter logo'
@@ -23,21 +63,13 @@ export default function SignIn({providers}) {
                 <p className="text-center text-sm italic my-10">This app is Twitter Clone for learning purpose</p>
                 <button
                 className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-                onClick={()=> signIn(provider.id,{callbackUrl:'/'})}
-                >Sign in with {provider.name}</button>
+                onClick={onGoogleClick}
+                >Sign in with Google</button>
 
                 </div>
-            ))}
+        
         </div>
     </div>
   )
 }
 
-export async function getServerSideProps(){
-    const providers =  await getProviders();
-    return {
-        props:{
-            providers,
-        }
-    }
-}
